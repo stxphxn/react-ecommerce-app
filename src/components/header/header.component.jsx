646822a956c422bbbe87d696979b101c1a064f15
Stephen <stephen.horsfall@atlascity.io>
2019-12-28 14:03:45 +0000

    add linting

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,15 +6,15 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 
 const Header = () => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
     </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
+    <div className="options">
+      <Link className="option" to="/shop">
         SHOP
       </Link>
-      <Link className='option' to='/shop'>
+      <Link className="option" to="/shop">
         CONTACT
       </Link>
     </div>
