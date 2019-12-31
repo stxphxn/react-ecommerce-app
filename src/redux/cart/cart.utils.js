caf1208b2bd006d4f592ff682a5051e68065dd7a
Stephen <stephen.horsfall@atlascity.io>
2019-12-31 13:05:10 +0000

    added addItem and removeItem to checkout-item

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,4 +1,3 @@
-/* eslint-disable import/prefer-default-export */
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   const existingCartItem = cartItems.find(
     (cartItem) => cartItem.id === cartItemToAdd.id,
@@ -11,4 +10,18 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   }
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id,
+  );
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  return cartItems.map((cartItem) => (cartItem.id === cartItemToRemove.id
+    ? { ...cartItem, quantity: cartItem.quantity - 1 }
+    : cartItem));
 };
