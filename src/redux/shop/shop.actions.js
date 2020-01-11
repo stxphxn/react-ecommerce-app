9aa499403f91cb84619248d46b22fd7c6cb27f9c
stxphxn <stephen.horsfall@atlascity.io>
2020-01-11 20:04:37 +0000

    move shop data to firebase and add spinner component

---
+++ src/redux/shop/shop.actions.js
@@ -0,0 +1,7 @@
+/* eslint-disable import/prefer-default-export */
+import ShopActionTypes from './shop.types';
+
+export const updateCollections = (collectionsMap) => ({
+  type: ShopActionTypes.UPDATE_COLLECTIONS,
+  payload: collectionsMap,
+});
